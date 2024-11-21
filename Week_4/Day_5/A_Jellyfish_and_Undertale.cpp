5090c6f6f4d968e54cefd96ee40ec4dba248c286
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2024-11-21 19:06:42 +0600

    Create A_Jellyfish_and_Undertale.cpp

---
+++ Week_4/Day_5/A_Jellyfish_and_Undertale.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const int mod = 1e9 + 7;
+#define pi 3.141592653
+#define ll long long int
+#define all(x) x.begin(),x.end()
+#define out(x) cout << x << "\n";
+#define ina(a,x,y) for(int i=x;i<y;i++){cin>>a[i];}
+#define inv(v) for(auto &it: v){cin >> it;}
+
+#define tc ll ttc; cin >> ttc; while(ttc--)
+#define IOS (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll mx, init, t; cin >> mx >> init >> t;
+    ll sum = 0;
+
+    for(int i=0; i<t; i++) {
+        ll x; cin >> x;
+
+        if(x < mx) {
+            sum += x;
+        } else {
+            sum += mx - 1;
+        }
+    } 
+
+    out(sum+init);
+}
+
+//_______________________________________________________________________________________________
+ 
+ 
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+ 
+ 
+//_______________________________________________________________________________________________
