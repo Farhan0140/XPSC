17def66889532044c1e4f0153df3990b62a124b0
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2024-11-26 22:52:32 +0600

    new

---
+++ Practice_Contest/Practice_Contest_4/C_Cookie_Day.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const int mod = 1e9 + 7;
+#define pi 3.141592653
+#define ll long long int
+#define all(x) x.begin(),x.end()
+#define out(x) cout<<x<<"\n";
+#define inv(v) for(auto &it: v){cin >> it;}
+
+#define tc ll ttc; cin >> ttc; while(ttc--)
+#define IOS (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+void in(ll &x) { cin >> x; }
+void in(ll &x, ll &y) { cin >> x >> y; }
+void in(ll &x, string &y) { cin >> x >> y; }
+void in(ll &x, ll &y, ll &z) { cin >> x >> y >> z; }
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n, c; in(n, c);
+    ll mn = INT_MAX;
+    bool bl = false;
+    while(n--) {
+        ll x; in(x);
+
+        if(x / c > 0) {
+            mn = min(mn, x % c);
+            bl = true;
+        }
+    }
+
+    if(bl){ out(mn); } else { out(-1); }
+}
+
+//_______________________________________________________________________________________________
+ 
+ 
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+ 
+ 
+//_______________________________________________________________________________________________
---
+++ Week_5/Day_4/F_BAN_BAN.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const int mod = 1e9 + 7;
+#define pi 3.141592653
+#define ll long long int
+#define all(x) x.begin(),x.end()
+#define out(x) cout<<x<<"\n";
+#define inv(v) for(auto &it: v){cin >> it;}
+
+#define tc ll ttc; cin >> ttc; while(ttc--)
+#define IOS (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+void in(ll &x) { cin >> x; }
+void in(ll &x, ll &y) { cin >> x >> y; }
+void in(ll &x, string &y) { cin >> x >> y; }
+void in(ll &x, ll &y, ll &z) { cin >> x >> y >> z; }
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n; in(n);
+
+    if(n%2==0) {
+        out((n/2));
+        ll x=1, y=n*3;
+
+        for(int i=0; i<n/2; i++) {
+            cout << x << " " << y << '\n';
+            x += 3; y -= 3;
+        }
+    } else {
+        out(((n/2)+1));
+        ll x=1, y=n*3;
+
+        for(int i=0; i<((n/2)+1); i++) {
+            cout << x << " " << y << '\n';
+            x += 3; y -= 3;
+        }
+    }
+}
+
+//_______________________________________________________________________________________________
+ 
+ 
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+ 
+ 
+//_______________________________________________________________________________________________
