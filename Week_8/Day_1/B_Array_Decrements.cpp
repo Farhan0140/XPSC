027c88a5f714f4d0d2a557a88ba7d07ff5bad801
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2024-12-16 00:41:19 +0600

    Create B_Array_Decrements.cpp

---
+++ Week_8/Day_1/B_Array_Decrements.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+void    in(ll &x)                               { cin >> x; }
+void    in(ll &x, ll &y)                        { cin >> x >> y; }
+void    in(ll &x, ll &y, ll &z)                 { cin >> x >> y >> z; }
+void    in(ll &x, ll &y, ll &z, ll &a)          { cin >> x >> y >> z >> a; }
+void    in(ll &x, string &y)                    { cin >> x >> y; }
+void    in(string &x)                           { cin >> x; }
+void    in(ll &x, string &y, string &z)         { cin >> x >> y >> z; }
+void    in(string &x, string &y)                { cin >> x >> y; }
+void    in(string &x, string &y, string &z)     { cin >> x >> y >> z; }
+void    in(vector<ll> &v) { for(auto &it: v)    { cin >> it; }}
+
+void    out()                                   { cout << "\n"; }
+void    out(int x)                              { cout << x << "\n"; }
+void    out(ll x)                               { cout << x << "\n"; }
+void    out(char x)                             { cout << x << "\n"; }
+void    out(double x)                           { cout << x << "\n"; }
+void    out(string x)                           { cout << x << "\n"; }
+void    out(int x, int y)                       { cout << x << " " << y << "\n"; }
+void    out(ll x, ll y)                         { cout << x << " " << y << "\n"; }
+void    out(auto x, auto y)                     { cout << x << " " << y << "\n"; }
+void    out(ll x, string y)                     { cout << x << " " << y << "\n"; }
+void    out(vector<ll> v)                       { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<char> v)                     { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<int> v)                      { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<string> v)                   { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n; in(n);
+    vi arr_1(n), arr_2(n); in(arr_1); in(arr_2);
+
+    bool is_possible = true;
+    ll mx = 0;
+    map<ll, int> mp;
+
+    for(int i=0; i<n; i++) {
+        if(arr_1[i] < arr_2[i]) {
+            is_possible = false;
+        } else {
+            if(arr_2[i] == 0) {
+                mx = max(mx, arr_1[i]);
+            } else {
+                ll diff = arr_1[i] - arr_2[i];
+                mp[diff]++;
+            }
+        }
+    }
+
+    if(mp.size() > 1) {
+        is_possible = false;
+    }
+
+    ll val = -1;
+    for(auto [x, y]: mp) {
+        val = x;
+    }
+
+    if(val != -1 && mx > val) {
+        is_possible = false;
+    }
+
+    (is_possible)? out("YES"): out("NO");
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
