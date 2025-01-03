444472a329eabdede336a6796ef1f54b4e487c74
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-03 21:06:46 +0600

    new

---
+++ Week_10/Day_6/Traffic_Lights.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+void    in(ll &x)                               { cin >> x; }
+void    in(ll &x, ll &y)                        { cin >> x >> y; }
+void    in(ll &x, ll &y, ll &z)                 { cin >> x >> y >> z; }
+void    in(ll &x, ll &y, ll &z, ll &a)          { cin >> x >> y >> z >> a; }
+void    in(ll &x, string &y)                    { cin >> x >> y; }
+void    in(string &x)                           { cin >> x; }
+void    in(ll &x, string &y, string &z)         { cin >> x >> y >> z; }
+void    in(string &x, string &y)                { cin >> x >> y; }
+void    in(string &x, string &y, string &z)     { cin >> x >> y >> z; }
+void    in(vector<ll> &v) { for(auto &it: v)    { cin >> it; }}
+
+void    out()                                   { cout << "\n"; }
+void    out(int x)                              { cout << x << "\n"; }
+void    out(ll x)                               { cout << x << "\n"; }
+void    out(char x)                             { cout << x << "\n"; }
+void    out(double x)                           { cout << x << "\n"; }
+void    out(string x)                           { cout << x << "\n"; }
+void    out(int x, int y)                       { cout << x << " " << y << "\n"; }
+void    out(ll x, ll y)                         { cout << x << " " << y << "\n"; }
+// void    out(auto x, auto y)                     { cout << x << " " << y << "\n"; }
+void    out(ll x, string y)                     { cout << x << " " << y << "\n"; }
+void    out(vector<ll> v)                       { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<char> v)                     { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<int> v)                      { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<string> v)                   { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+
+
+ll gcd(ll a, ll b) {
+    return __gcd(a, b);
+}
+
+ll lcm(ll a, ll b) {
+    return ((a / __gcd(a, b)) * b);
+}
+
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n, k; in(n, k);
+
+    set<ll> pos;
+    multiset<ll> len;
+
+    pos.insert(0); pos.insert(n);
+    len.insert(n - 0);
+
+    for(int i=0; i<k; i++) {
+        ll x; in(x);
+        pos.insert(x);
+
+        auto it = pos.find(x);
+        ll prev_val = *prev(it);
+        ll nxt_val = *next(it);
+
+        len.erase(len.find(nxt_val - prev_val));
+
+        len.insert(x - prev_val);
+        len.insert(nxt_val - x);
+
+        cout << *len.rbegin() << " ";
+    }
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    tc
+    */
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
