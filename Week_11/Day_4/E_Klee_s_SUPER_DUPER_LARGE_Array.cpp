fe911e9fa33bb80a92fb0dc0e624f983fb6bda26
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-09 22:55:51 +0600

    new

---
+++ Week_11/Day_4/E_Klee_s_SUPER_DUPER_LARGE_Array.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+void    in(ll &x)                               { cin >> x; }
+void    in(ll &x, ll &y)                        { cin >> x >> y; }
+void    in(ll &x, ll &y, ll &z)                 { cin >> x >> y >> z; }
+void    in(ll &x, ll &y, ll &z, ll &a)          { cin >> x >> y >> z >> a; }
+void    in(ll &x, string &y)                    { cin >> x >> y; }
+void    in(string &x)                           { cin >> x; }
+void    in(ll &x, string &y, string &z)         { cin >> x >> y >> z; }
+void    in(string &x, string &y)                { cin >> x >> y; }
+void    in(string &x, string &y, string &z)     { cin >> x >> y >> z; }
+void    in(vector<ll> &v) { for(auto &it: v)    { cin >> it; }}
+
+void    out()                                   { cout << "\n"; }
+void    out(int x)                              { cout << x << "\n"; }
+void    out(ll x)                               { cout << x << "\n"; }
+void    out(char x)                             { cout << x << "\n"; }
+void    out(double x)                           { cout << x << "\n"; }
+void    out(string x)                           { cout << x << "\n"; }
+void    out(int x, int y)                       { cout << x << " " << y << "\n"; }
+void    out(ll x, ll y)                         { cout << x << " " << y << "\n"; }
+void    out(ll x, string y)                     { cout << x << " " << y << "\n"; }
+void    out(vector<ll> v)                       { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<char> v)                     { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<int> v)                      { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<string> v)                   { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+
+
+ll gcd(ll a, ll b) {
+    return __gcd(a, b);
+}
+
+ll lcm(ll a, ll b) {
+    return ((a / __gcd(a, b)) * b);
+}
+
+
+// Sum of n terms in Arithmetic Progression ---> S = n/2[2a + (n − 1) × d]
+ll get_sum(ll a, ll n) {
+    ll x = n * (((2 * a) + (n - 1)) * 1);
+
+    return x / 2;
+}
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n, k; in(n, k);
+
+    ll l = 1, r = n, ans = INT_MAX;
+    while(l <= r) {
+        ll mid = l + (r - l) / 2;
+
+        ll left_sum = get_sum(k, mid), right_sum = get_sum(k+mid, n-mid);
+        ans = min(ans, abs(left_sum - right_sum));
+
+        if(left_sum <= right_sum) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    out(ans);
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
