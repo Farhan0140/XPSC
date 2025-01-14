ae52f7eeb74ec937ff888efaafc9bf36977d2243
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-15 01:22:05 +0600

    Create A_Almost_Prime.cpp

---
+++ Week_12/Day_2/A_Almost_Prime.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define pb                                      push_back
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+template<typename TMP_BRO, typename... Yo_Nadim>
+void in(TMP_BRO &x, Yo_Nadim&... nadim) { cin >> x; if constexpr (sizeof...(nadim) > 0) { in(nadim...);}}
+template<typename TMP_BRO>
+void in(vector<TMP_BRO> &v) {for (auto &it : v) {cin >> it;}}
+template<typename TMP_BRO>
+void out(const TMP_BRO &x) {cout << x << "\n";}
+template<typename TMP_BRO, typename... Yo_Nadim>
+void out(const TMP_BRO &x, const Yo_Nadim&... nadim) {cout << x << " ";out(nadim...);}
+template<typename TMP_BRO>
+void out(const vector<TMP_BRO> &v) {for (const auto &it : v) {cout << it << " ";}cout << "\n";}
+void out() { cout << "\n"; }
+ll gcd(ll a, ll b) { return __gcd(a, b);}
+ll lcm(ll a, ll b) {return ((a / __gcd(a, b)) * b);}
+
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n; in(n);
+    auto t_prime = [](ll x) {
+        ll cnt = 0;
+        for(int i=2; i*i<=x; i++) {
+            if(x%i == 0) {
+                while(x%i == 0) {
+                    x /= i;
+                }
+                cnt++;
+            }
+        }
+        if(x > 1) {
+            cnt++;
+        }
+        return cnt;
+    };
+
+    ll cnt = 0;
+
+    for(int i=1; i<=n; i++) {
+        if(t_prime(i) == 2) {
+            cnt++;
+        }
+    }
+
+    out(cnt);
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    tc
+    */
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
