497e59a46dc6821cdf7d9e04531fccf3fd699461
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-18 23:17:27 +0600

    Create B_Sherlock_and_his_girlfriend.cpp

---
+++ Week_12/Day_6/B_Sherlock_and_his_girlfriend.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define pb                                      push_back
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+template<typename TMP_BRO, typename... Yo_Nadim>
+void in(TMP_BRO &x, Yo_Nadim&... nadim) { cin >> x; if constexpr (sizeof...(nadim) > 0) { in(nadim...);}}
+template<typename TMP_BRO>
+void in(vector<TMP_BRO> &v) {for (auto &it : v) {cin >> it;}}
+template<typename TMP_BRO>
+void out(const TMP_BRO &x) {cout << x << "\n";}
+template<typename TMP_BRO, typename... Yo_Nadim>
+void out(const TMP_BRO &x, const Yo_Nadim&... nadim) {cout << x << " ";out(nadim...);}
+template<typename TMP_BRO>
+void out(const vector<TMP_BRO> &v) {for (const auto &it : v) {cout << it << " ";}cout << "\n";}
+void out() { cout << "\n"; }
+ll gcd(ll a, ll b) { return __gcd(a, b);}
+ll lcm(ll a, ll b) {return ((a / __gcd(a, b)) * b);}
+
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n; in(n);
+    vector<bool> arr(n+50, true);
+
+    for(int i=2; i<=n+1; i++) {
+        if(arr[i]) {
+            for(int j=i+i; j<=n+1; j+=i) {
+                arr[j] = false;
+            }
+        }
+    }
+
+    (n <= 2)? out(1): out(2);
+
+    for(int i=2; i<=n+1; i++) {
+        if(arr[i]) {
+            cout << 1 << " ";
+        } else {
+            cout << 2 << " ";
+        }
+    }
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    tc
+    */
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
