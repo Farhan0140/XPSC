e381d80b376a49861441db7da6650e6d843f197b
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-18 23:17:35 +0600

    Create C_Insert_and_Equalize.cpp

---
+++ Week_12/Day_6/C_Insert_and_Equalize.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define pb                                      push_back
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+template<typename TMP_BRO, typename... Yo_Nadim>
+void in(TMP_BRO &x, Yo_Nadim&... nadim) { cin >> x; if constexpr (sizeof...(nadim) > 0) { in(nadim...);}}
+template<typename TMP_BRO>
+void in(vector<TMP_BRO> &v) {for (auto &it : v) {cin >> it;}}
+template<typename TMP_BRO>
+void out(const TMP_BRO &x) {cout << x << "\n";}
+template<typename TMP_BRO, typename... Yo_Nadim>
+void out(const TMP_BRO &x, const Yo_Nadim&... nadim) {cout << x << " ";out(nadim...);}
+template<typename TMP_BRO>
+void out(const vector<TMP_BRO> &v) {for (const auto &it : v) {cout << it << " ";}cout << "\n";}
+void out() { cout << "\n"; }
+ll lcm(ll a, ll b) {return ((a / __gcd(a, b)) * b);}
+
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n; in(n);
+    vi arr(n);
+    map<ll, int> mp;
+
+    for(auto &it: arr) {
+        in(it);
+        mp[it]++;
+    }
+
+    if(n == 1) {
+        out(1); return;
+    }
+    sort(all(arr), greater<ll>());
+
+    ll mx = arr[0];
+    vi dif;
+    for(int i=0; i<n; i++) {
+        dif.push_back(mx - arr[i]);
+    }
+
+    ll Gcd = dif[0];
+    for(int i=1; i<n; i++) {
+        Gcd = __gcd(Gcd, dif[i]);
+    }
+
+    ll ans = 0;
+    for(int i=0; i<n; i++) {
+        ans += dif[i] / Gcd;
+    }
+
+    ll cur = arr[0];
+    while(mp.count(cur)) {
+        cur -= Gcd;
+    }
+
+    ans += (arr[0] - cur) / Gcd;
+
+    out(ans);
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
