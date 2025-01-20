32390f2b799099304eab675088d18015609f23e0
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-20 22:47:43 +0600

    Create A_Kevin_and_Arithmetic.cpp

---
+++ Practice_Contest/CodeForces/A_Kevin_and_Arithmetic.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define pb                                      push_back
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+//_______________________________________________________________________________________________
+
+
+template<typename TMP_BRO, typename... Yo_Nadim>
+void in(TMP_BRO &x, Yo_Nadim&... nadim) { cin >> x; if constexpr (sizeof...(nadim) > 0) { in(nadim...);}}
+template<typename TMP_BRO>
+void in(vector<TMP_BRO> &v) {for (auto &it : v) {cin >> it;}}
+template<typename TMP_BRO>
+void out(const TMP_BRO &x) {cout << x << "\n";}
+template<typename TMP_BRO, typename... Yo_Nadim>
+void out(const TMP_BRO &x, const Yo_Nadim&... nadim) {cout << x << " ";out(nadim...);}
+template<typename TMP_BRO>
+void out(const vector<TMP_BRO> &v) {for (const auto &it : v) {cout << it << " ";}cout << "\n";}
+void out() { cout << "\n"; }
+ll gcd(ll a, ll b) { return __gcd(a, b);}
+ll lcm(ll a, ll b) { return ((a / __gcd(a, b)) * b);}
+
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n; in(n);
+    vi arr(n); in(arr);
+
+    vi e, o;
+    for(auto it: arr) {
+        if(it & 1) {
+            o.pb(it);
+        } else {
+            e.pb(it);
+        }
+    }
+
+    ll cnt = 0, sum = 0; 
+    for(auto it: e) {
+        sum += it;
+
+        if(sum % 2 == 0) {
+            cnt++;
+
+            while(sum % 2 == 0) {
+                sum >>= 1;
+            }
+        }
+    }
+    for(auto it: o) {
+        sum += it;
+
+        if(sum % 2 == 0) {
+            cnt++;
+
+            while(sum % 2 == 0) {
+                sum >>= 1;
+            }
+        }
+    }
+
+    out(cnt);
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    */
+    tc
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
